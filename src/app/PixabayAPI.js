fdb78ad3cf946eecd769d6c2a463e10cda8a9ecd
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-06-15 20:10:38 +0300

    finish api Pixabay

--- src/app/PixabayAPI.js
+++ src/app/PixabayAPI.js
@@ -13,10 +13,31 @@ export class PixabayAPI {
     const params = {
       page: this.#page,
       per_page: this.#per_page,
-      query: this.#query,
+      q: this.#query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
     };
+    const URL_AXIOS = `?key=${API_PIXABAY_KEY}`;
+    const { data } = await axios.get(URL_AXIOS, { params });
+    return data;
+  }
+  get query() {
+    this.#query;
+  }
+  set query(newQuery) {
+    this.#query = newQuery;
+  }
+  incrementPage() {
+    this.#page += 1;
+  }
+  resetPage() {
+    this.#page = 1;
+  }
+  setTotal(total) {
+    this.#totalPages = total;
+  }
+  hasMorePhotos() {
+    return this.#page < Math.ceil(this.#totalPages / this.#per_page);
   }
 }
