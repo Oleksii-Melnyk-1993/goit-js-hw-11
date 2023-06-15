04cc655dd532b07526f36c2d60cf8b9c2ee7e0fb
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-06-15 22:40:11 +0300

    final changes before make main code

---
+++ src/app/refs.js
@@ -0,0 +1,12 @@
+const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  btnLoadMore: document.querySelector('.load-more'),
+  backdrop: document.querySelector('.backdrop'),
+  spinner: document.querySelector('.js-spinner'),
+  body: document.querySelector('body'),
+  btnUp: document.getElementById('to-top-btn'),
+  btnWrapper: document.querySelector('.btn-up'),
+  like: document.querySelector('.heart'),
+  searchInput: document.querySelector('.search-form-input'),
+};
